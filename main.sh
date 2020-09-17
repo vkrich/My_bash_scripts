723993a2052c2fd8a33430406ba040b490b010b3
vkrich <vladislav.krichevsky@mail.ru>
2020-09-17 11:07:19 +0000

    Add file output and reading

--- main.sh
+++ main.sh
@@ -3,9 +3,13 @@ path=~/
 arg1=$1
 arg2=$2
 
-echo "Hello ${path} !"
-echo '${path}'
-echo "\$path"
-echo "{path}"
-echo "My script $0 catch - $arg1 as first arg and $arg2 as second arg"
-echo "Arguments are: \$1=$arg1 \$2=$arg2"
+touch a.out
+
+echo "Hello ${path} !" > a.out
+echo '${path}' >> a.out
+echo "\$path" >> a.out
+echo "{path}" >> a.out
+echo "My script $0 catch - $arg1 as first arg and $arg2 as second arg" >> a.out
+echo "Arguments are: \$1=$arg1 \$2=$arg2" >> a.out
+
+less a.out
